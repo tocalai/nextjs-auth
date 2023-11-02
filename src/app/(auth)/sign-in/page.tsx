b01b3ff5c4963d00dde714c037ea30cb92cf99bc
Danny Lai <tocalai>
2023-11-02 21:00:06 +0800

    Add sign in form ui

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='w-full flex'>
+    <div className='w-full'>
         <SignInForm />
     </div>
   )
--- src/app/components/form/SignInForm.tsx
+++ src/app/components/form/SignInForm.tsx
@@ -1,9 +1,72 @@
-import React from 'react'
+'use client'
+import { Button } from '@/components/ui/button'
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from "react-hook-form"
+import * as z from "zod"
 
-const SignInForm = () => {
-  return (
-    <div>SignInForm</div>
-  )
+
+const FormSchema = z.object({
+  email: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+  password: z.string()
+})
+
+
+const onSubmit = () => {
+
 }
 
-export default SignInForm
+export default function SignInForm() {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      email: "",
+    },
+  })
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-full">
+        <div className='space-y-3'>
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>email</FormLabel>
+                <FormControl>
+                  <Input placeholder="Input your email" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>password</FormLabel>
+                <FormControl>
+                  <Input placeholder="Input your password" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
+        <Button className='w-full mt-5' type="submit">Sign In</Button>
+      </form>
+      <div className='mx-auto my-4 flex w-full items-center justify-evenly before:mr-4 before: block before: h-px before: flex-grow before: bg-stone-400 after:ml-4 after:block after: h-px after: flex-grow after:bg-stone-400'>
+        or
+      </div>
+      <p className='text-center text-sm text-gray-600 mt-2'>
+         If you don&apos;t have an account, please&nbsp;       
+      </p>
+    </Form>
+  )
+}
