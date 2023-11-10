771d125195affff690e6f62b7bc802db96aa3e4d
Danny Lai <tocalai>
2023-11-10 23:35:15 +0800

    Add rich mail temaplte.

--- src/app/api/mail/route.ts
+++ src/app/api/mail/route.ts
@@ -6,16 +6,16 @@ import { Resend } from 'resend';
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST() {
-  try {
-    const data = await resend.emails.send({
-      from: 'Acme <onboarding@resend.dev>',
-      to: ['delivered@resend.dev'],
-      subject: 'Hello world',
-      react: MailTemplate({ username: 'John' }),
-    });
+    try {
+        const data = await resend.emails.send({
+            from: 'Acme <onboarding@resend.dev>',
+            to: ['delivered@resend.dev'],
+            subject: 'Hello world',
+            react: MailTemplate({ username: 'John' }),
+        });
 
-    return NextResponse.json(data);
-  } catch (error) {
-    return NextResponse.json({ error });
-  }
+        return NextResponse.json(data);
+    } catch (error) {
+        return NextResponse.json({ error });
+    }
 }
--- src/app/components/mail/MailTemplate.tsx
+++ src/app/components/mail/MailTemplate.tsx
@@ -1,15 +1,50 @@
 import React from 'react'
+import {
+    Body,
+    Button,
+    Container,
+    Head,
+    Hr,
+    Html,
+    Preview,
+    Section,
+    Text,
+} from '@react-email/components';
+import { Tailwind } from '@react-email/tailwind';
 
 interface MailTemplateProps {
     username: string;
-  }
+}
 
 const MailTemplate = ({
-   username
-} : MailTemplateProps) => {
-  return (
-    <div>Welcome, {username}</div>
-  )
+    username
+}: MailTemplateProps) => {
+    return (
+        <Html>
+            <Head />
+            <Preview>Hello, {username}, Please verfiy your account.</Preview>
+            <Tailwind>
+                <Body>
+                    <Container>
+                        <Section>
+                            <Hr />
+                            <Text>
+                                Welcome to onboard. Please verfiy and active your account.
+                            </Text>
+                            <Button
+                                href="https://dashboard.stripe.com/login"
+                            >
+                                Verify Your Account
+                            </Button>
+                            <Hr />
+                            <Text>
+                            </Text>
+                        </Section>
+                    </Container>
+                </Body>
+            </Tailwind>
+        </Html>
+    )
 }
 
 export default MailTemplate
