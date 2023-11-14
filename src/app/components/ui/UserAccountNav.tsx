0986b1c0554b12acbcf508e1e94df8e290fd3cd3
Danny Lai <tocalai>
2023-11-14 20:57:28 +0800

    Update user nav UI

--- src/app/components/ui/UserAccountNav.tsx
+++ src/app/components/ui/UserAccountNav.tsx
@@ -2,15 +2,16 @@
 'use client'
 
 import { Button } from "@/components/ui/button"
-import { Separator } from "@radix-ui/react-separator"
 import { signOut } from "next-auth/react"
 import ResetPasswordDialog from "./ResetPasswordDialog"
+import { Separator } from "@/components/ui/separator"
 
 const UserAccountNav = () => {
   return (
     <>
+      <div>Profie</div>
       <ResetPasswordDialog/>
-      {/* <Separator orientation="vertical"/> */}
+      <Separator orientation="vertical"/>
       <Button onClick={() => signOut({
         redirect: true,
         callbackUrl: `${window.location.origin}/sign-in`
