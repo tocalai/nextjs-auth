b5c30ae088c884f67beb018606905be4b0697fdd
Danny Lai <tocalai>
2023-11-27 21:14:17 +0800

    User page force to no cache.

--- src/app/(dashboard)/admin/user/page.tsx
+++ src/app/(dashboard)/admin/user/page.tsx
@@ -15,6 +15,7 @@ import { User } from ".prisma/client"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import { UserStatistic } from "@/types/user"
 
+export const revalidate = 0 // no cache
 
 interface IndexPageProps {
   searchParams: {
