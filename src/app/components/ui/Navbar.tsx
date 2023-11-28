f3da794e1020805ff5be2d58f8f4fe6321518bac
Danny Lai <tocalai>
2023-11-28 20:17:39 +0800

    Add dashboard nav icon.

--- src/app/components/ui/Navbar.tsx
+++ src/app/components/ui/Navbar.tsx
@@ -1,5 +1,5 @@
 import { Button, buttonVariants } from "@/components/ui/button";
-import { PlaneTakeoff } from "lucide-react";
+import { CandlestickChart, PlaneTakeoff } from "lucide-react";
 import { getServerSession } from "next-auth";
 import Link from "next/link";
 import UserAccountNav from "./UserAccountNav";
@@ -13,7 +13,12 @@ export default async function Navbar() {
     <>
       <div className="py-2 border-b fixed w-full top-1">
         <div className="container flex items-center justify-between">
-          <Link href="/" ><PlaneTakeoff width={50} height={50} /></Link>
+          <div className="flex items-start">
+            <Link href="/" ><PlaneTakeoff width={35} height={35} /></Link>
+            {session?.user && (
+              <Link className="ml-3 mt-1" href='/admin/user' as={`/admin/user?page=1&per_page=${process.env.NEXT_PUBLIC_USER_STATISTICS_TABLE_ROWS_PER_PAGE}`} ><CandlestickChart width={35} height={35} /></Link>
+            )}
+          </div>
           <div className="flex h-5 items-center space-x-4">
             {session?.user ? (
               <UserAccountNav />
