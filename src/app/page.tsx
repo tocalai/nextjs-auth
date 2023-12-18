fc9fa4e05e387296544e79af2bed30d799fcbd68
Danny Lai <danny_lai@kingston.com.tw>
2023-12-18 08:28:24 +0800

    Test force dynamic.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { authOptions } from '@/lib/auth'
 import { Hand } from 'lucide-react'
 import { getServerSession } from 'next-auth'
 
-export const dynamic = 'auto'
+export const dynamic = 'force-dynamic'
 
 export default async function Home() {
   const session = await getServerSession(authOptions)
